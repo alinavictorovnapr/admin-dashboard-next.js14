4c99856f5928875f013ccf881006ece1d90509b0
alprokopovich <alprokopovich@ecisys.com>
2024-01-31 16:25:54 +0200

    Admin Dashboard using Next14 env

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -6,7 +6,7 @@ export const connectToDB = async () =>{
     try {
         if (connection.isConnected) return;
         const db = await mongoose.connect(
-            process.env.MONGO
+            process.env.MONGODB_URI
         );
         connection.isConnected = db.connections[0].readyState;
     } catch (error) {
